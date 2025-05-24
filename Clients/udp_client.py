5ffb47d9c0c2b457296944a1feddc1ee877e4f1f
Med Amine <mohamedaminebenfakir@gmail.com>
2025-05-24 03:00:46 +0100

    Completed udp and tcp clients and giving another project structure

---
+++ Clients/udp_client.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+client.sendto(b"SENTmessage", (target_host, target_port))
+
+
+data, addr = client.recvfrom(4096)
+
+print(data.decode())
+
+
+client.close()
